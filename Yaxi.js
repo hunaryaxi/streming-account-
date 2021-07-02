9c253f935f14e7b130a0c10da5581b48a4ad016f
Yaxi721 <77636257+hunaryaxi@users.noreply.github.com>
2021-07-02 11:11:59 +0300

    Update Yaxi.js

--- Yaxi.js
+++ Yaxi.js
@@ -1,1 +1,28 @@
 
+const Discord = require("discord.js");
+const jano = new Discord.Client();
+//////// playing or dnd  streaming ////////
+Yaxi.on("ready", () => {
+  console.log(
+    `Online In Servers`
+  );
+  let statuses = [
+  
+  `You only live once, so try to live for God.`,
+ 
+  ];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    Yaxi.user.setActivity(STREAMING, {      
+    type: "STREAMING", 
+    url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+///////////////////////
+Yaxi.on("ready", () => {
+    var join = Yaxi.channels.get(""); /// id voice
+    if (join) join.join();
+  });
+//////////////// tokn lera da bne 
+Yaxi.login("");
