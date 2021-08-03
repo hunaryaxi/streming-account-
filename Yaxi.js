90940f669a5fc8c5f4cb5e3cd7be10645f7f0133
HJupBERLIN <80848798+HJupBERLIN@users.noreply.github.com>
2021-08-04 02:05:22 +0300

    Update Yaxi.js

--- Yaxi.js
+++ Yaxi.js
@@ -25,4 +25,4 @@ Yaxi.on("ready", () => {
     if (join) join.join();
   });
 //////////////// tokn lera da bne 
-Yaxi.login("");
+Yaxi.login("mfa.NHg8Bk-_ZWFNH12O1GO5d_Xmv8nTIL3tbOl3JX6Uh2dXkkX0JKFmBhb2KEw-_Exorl92d69W_eXGHZA2r007");
